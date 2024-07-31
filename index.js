3c8afdc5a39a756675f8ce2d8b6eaf94d1b89a1b
YASH YADAV <75584422+ALPHAWINS02@users.noreply.github.com>
2024-07-31 22:16:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors=require('cors');
 const dotenv = require('dotenv');
 
 const userRoutes = require('./src/routes/User');
@@ -13,14 +13,9 @@ const errorHandler = require('./src/middlewares/errorHandler');
 const app = express();
 dotenv.config();
 
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,10 +23,6 @@ app.use(bodyParser.json());
 app.use(userRoutes);
 app.use(folderRoutes);
 app.use(formRoutes);
-
-app.get('/', (req, res) => {
-    res.status(200).send({ status: "success", msg: "API is working well." });
-})
 
 app.use((req, res, next) => {
     const err = Object.assign(Error("Endpoint not found"), { code: 404 });
@@ -42,5 +33,7 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 1000 
 app.listen(port, () => {
-    mongoose.connect(process.env.DB_URL).then(() => console.log('Server is running :)')).catch((error) => console.log(error))
+    mongoose.connect(process.env.DB_URL).then(() => app.get('/', (req, res) => {
+        res.status(200).send({ status: "success", msg: "API is working well." });
+    })).catch((error) => console.log(error))
 })
